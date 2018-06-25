dda14b009f7473f25b8cdb8803d3c2d51f7f44cf
Joshua Peterson <peterson.c.joshua@gmail.com>
2018-06-25 14:40:54 -0700

    little improvements

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ali-feature-extractor
 Pretrained CIFAR10 ALI GAN with code for feature extraction (no training code)
+
+Use this with the main repo
--- ali_feature_extractor.py
+++ ali_feature_extractor.py
@@ -14,20 +14,23 @@ import pickle as p
 
 from ali import streams
 
-#code to pull images from cifar10 image data set
-# _,_, data_stream = streams.create_cifar10_data_streams(64,64,numpy.random.RandomState())
-# examples, = next(data_stream.get_epoch_iterator())
 
-def image_to_z(main_loop,image_name):
+
+
+def image_to_z(main_loop, image):
+
+    """ Perform inference given an image filename. 
+    """
+
     ali, = main_loop.model.top_bricks
 
-    image = [misc.imread(image_name)]
-    image = (numpy.array(image)/255.0).transpose(0,3,1,2)
     x = tensor.tensor4('features')
 
     params = theano.function([x], (ali.encoder.mapping.apply(x)), allow_input_downcast=True)(image)
     mu, log_sigma = params[:, :ali.encoder._nlat], params[:, ali.encoder._nlat:]
     return mu
+
+
 
 def z_to_image(main_loop,z_mu_vector):
     ali, = main_loop.model.top_bricks
@@ -35,15 +38,27 @@ def z_to_image(main_loop,z_mu_vector):
     finalImage = theano.function([], decode)()
     return finalImage[0].transpose(1,2,0).squeeze()
 
+
+
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Plot reconstructions.")
+    parser = argparse.ArgumentParser(description="Feature extraction using ALI GAN.")
     parser.add_argument("main_loop_path", type=str,
                         help="path to the pickled main loop.")
-    parser.add_argument("image_name", type=str,help="image to reonstruct")
     args = parser.parse_args()
+
+
+    image = [misc.imread('1.png')]
+    print image.shape
+    image = (numpy.array(image)/255.0).transpose(0,3,1,2)
+    print image.shape
+    exit()
+
     with open(args.main_loop_path, 'rb') as src:
         main_loop = load(src)
 
     z_vector = image_to_z(main_loop, args.image_name)
-    finalImage = z_to_image(main_loop, z_vector)
-    misc.imsave("6r.png",finalImage)
+
+    #finalImage = z_to_image(main_loop, z_vector)
+    #misc.imsave("6r.png",finalImage)
+
+
